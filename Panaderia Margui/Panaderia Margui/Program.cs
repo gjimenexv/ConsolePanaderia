4317a97a9747163cebb01decf3c26fb26940fa74
Guillermo Jimenez <gjimenez30305@ufide.ac.cr>
2020-06-26 20:29:45 -0600

    Primer Commit del Proyecto

---
+++ Panaderia Margui/Panaderia Margui/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panaderia_Margui
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DoJob();
+        }
+        static void DoJob()
+        {
+            var elTrabajador = new Trabajador();
+            elTrabajador.Trabajo01();
+        }
+
+    }
+}
---
+++ Panaderia Margui/Panaderia Margui/Trabajador.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Panaderia_Margui
+{
+    internal class Trabajador
+    {
+            public void Trabajo01()
+            {
+                var laOpcion = string.Empty;
+            while (laOpcion != "x")
+                {
+                    DesplegarMenu();
+                    laOpcion = LeaLaOpcion();
+                }
+            }
+
+            private void DesplegarMenu()
+            {
+                Console.WriteLine("Menu Principal");
+                Console.WriteLine("1. Pan Dulce.");
+                Console.WriteLine("2. Pan Dulce.");
+                Console.WriteLine("3. Pan Artesanal.");
+                Console.WriteLine("x.  Salir");
+            }
+
+            private string LeaLaOpcion()
+            {
+                string elResultado = Console.ReadLine().ToLower();
+                return elResultado;
+            }
+    }
+}
